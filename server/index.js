dea5b4a2f37b4aa10b870e8a83e5a09e2695f01c
Kshitiz Gupta <kshitiz.gupta2@keysight.com>
2024-02-12 11:31:25 +0530

    updated resume

--- server/index.js
+++ server/index.js
@@ -21,14 +21,14 @@ app.post('/contact',(req,res,next)=>{
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-        user: 'chintu.sps@gmail.com',//replace with your email
-        pass: 'telekinesis'//replace with your password
+        user: process.env.email,//replace with your email
+        pass: process.env.password//replace with your password
         }
     });
     
     const mailOptions = {
-        from: 'chintu.sps@gmail.com',//replace with your email
-        to: 'chintu.sps@gmail.com',//replace with your email
+        from: process.env.email,//replace with your email
+        to: process.env.email,//replace with your email
         subject: `Contact name: ${req.body.firstName} ${req.body.lastName}`,
         html: `<h1>Contact details</h1>
             <h2> Name: ${req.body.firstName} ${req.body.lastName} </h2><br>
